336b193f8646b86cd42093053f469c8aa000e8b6
vincent <1339301794@qq.com>
2025-10-04 06:17:58 +0800

    1

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Main from './stock/index'
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Main />
-  </React.StrictMode>
+  <Main />
 );
